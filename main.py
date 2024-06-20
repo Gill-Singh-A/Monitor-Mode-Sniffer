3282daa1e55231d1055c202db62902d62c74a912
Gill-Singh-A <amansg22@iitk.ac.in>
2024-06-21 04:16:10 +0530

    Standard Functions Initialized and Modules/Packages Imported

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 #! /usr/bin/env python3
 
+from os import geteuid
+from scapy.all import *
+from datetime import date
+from optparse import OptionParser
+from colorama import Fore, Back, Style
+from time import strftime, localtime, sleep, time
+
+status_color = {
+    '+': Fore.GREEN,
+    '-': Fore.RED,
+    '*': Fore.YELLOW,
+    ':': Fore.CYAN,
+    ' ': Fore.WHITE
+}
+
+def display(status, data, start='', end='\n'):
+    print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
+
+def get_arguments(*args):
+	parser = OptionParser()
+	for arg in args:
+		parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
+	return parser.parse_args()[0]
+
+def check_root():
+	return geteuid() == 0
+
 if __name__ == "__main__":
-    pass
+	pass
