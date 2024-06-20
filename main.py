91dc453953735219c69328dcd6714cc4561ef99d
Gill-Singh-A <amansg22@iitk.ac.in>
2024-06-21 04:28:08 +0530

    Added Command Line Arguments

--- main.py
+++ main.py
@@ -19,13 +19,26 @@ def display(status, data, start='', end='\n'):
     print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
 
 def get_arguments(*args):
-	parser = OptionParser()
-	for arg in args:
-		parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
-	return parser.parse_args()[0]
+    parser = OptionParser()
+    for arg in args:
+        parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
+    return parser.parse_args()[0]
 
 def check_root():
-	return geteuid() == 0
+    return geteuid() == 0
 
 if __name__ == "__main__":
-	pass
+    arguments = get_arguments(('-i', "--interface", "interface", "Network Interface to Start Sniffing on"),
+                              ('-c', "--channel", "channel", "Channels to Sniff on (Seperated by ',' if multiple, Default=Channel Hopping)"),
+                              ('-w', "--write", "write", "Dump Packets to a File"))
+    if not check_root():
+        display('-', f"This Program requires {Back.YELLOW}root{Back.RESET} Privileges")
+        exit(0)
+    if not arguments.interface:
+        display('-', "Please specify an Interface")
+        display('*', f"Available Interfaces : {Back.MAGENTA}{get_if_list()}{Back.RESET}")
+        exit(0)
+    if not arguments.channel:
+        arguments.channel = [channel for channel in range(1, 15)]
+    else:
+        arguments.channel = arguments.channel.split(',')
